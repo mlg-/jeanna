aba59550acb474050707f1fd232eb5e590ddfdf4
MLG <melissaleighgore@gmail.com>
2014-12-05 18:15:24 -0500

    TWEAAAAKS

--- wp-content/themes/bluestockings/Landing Page.php
+++ wp-content/themes/bluestockings/Landing Page.php
@@ -15,7 +15,7 @@ get_header(); ?>
 
 	<div id="primary" class="content-area">
    <main id="main" class="site-main" role="main">
-				<div class="jumbotron-wrapper"><h1 class="coming-soon">COMING SOON</h1></div>
+				<div class="jumbotron-wrapper"><h1 class="coming-soon">C O M I N G &nbsp; S O O N</h1></div>
 					<div class="inner-jumbotron-container">
 						<div class="social">
  						<a href="http://t.co/ugJ9dISkf8"><img class="facebook" src="wp-content/themes/bluestockings/images/facebook.png"/></a>
@@ -39,7 +39,7 @@ get_header(); ?>
 			<div id="mc_embed_signup">
 			<form action="//bluestockingsboutique.us9.list-manage.com/subscribe/post?u=85aa1bdc14611c28a3a893aa9&amp;id=ebdf8ac3bf" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate" target="_blank" novalidate>
 			    <div id="mc_embed_signup_scroll">
-				<label for="mce-EMAIL">BE IN THE KNOW:&nbsp;&nbsp;&nbsp;</label>
+				<label for="mce-EMAIL">BE IN THE KNOW</label><br>
 				<input type="email" value="" name="EMAIL" class="email" id="mce-EMAIL" placeholder="EMAIL ADDRESS" required>&nbsp;&nbsp;&nbsp;&nbsp;
 			    <!-- real people should not fill this in and expect good things - do not remove this or risk form bot signups-->
 			    <div style="position: absolute; left: -5000px;"><input type="text" name="b_85aa1bdc14611c28a3a893aa9_ebdf8ac3bf" tabindex="-1" value=""></div>
@@ -49,13 +49,8 @@ get_header(); ?>
 			<br/>
 			</div>
 		</div><!-- .mailchimp--><br><br>
-				</div>
-			</div>
-			<br/>
-</div>
-</div>
-<!--End mc_embed_signup-->
 
+<!--End mc_embed_signup-->
 		</main><!-- #main -->
 	</div><!-- #primary -->
 <?php get_footer(); ?>
